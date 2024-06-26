15e23ba51fe0337c2c797432a22955976aeb61b3
gabbyraw <gabrielchibueze@gmail.com>
2024-06-26 08:35:30 +0100

    updated

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
-    credentials: true // Allow credentials to be sent in the request
+    // credentials: true // Allow credentials to be sent in the request
 };
 
 const accessLogStreams = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" })
